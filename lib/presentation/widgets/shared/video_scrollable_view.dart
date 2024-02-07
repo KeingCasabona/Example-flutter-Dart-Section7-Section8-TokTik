3370830907d345ace38d2bd3c346ddb34692e2a1
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-02-06 22:13:06 -0500

    feat: Add like buttons and views

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -10,16 +11,26 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       physics: BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Color.fromARGB(255, 108, 50, 107)),
-        Container(color: Color.fromARGB(255, 61, 156, 77)),
-        Container(color: Color.fromARGB(255, 84, 163, 172)),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //video player + gradiente
+
+            //Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
     );
   }
 }
